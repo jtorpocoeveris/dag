2333363e47b5975b53b01215ac296ad667d172c9
jherranlarosa <53498766+jherranlarosa@users.noreply.github.com>
2021-07-07 13:24:28 -0500

    Update puller.py

--- dags/puller.py
+++ dags/puller.py
@@ -16,36 +16,7 @@ import pandas as pd
 # config = open("config.json","r")
 # config = json.loads(config.read())
 # config = config[0]
-config = [
-  {
-          "route_trunk": "data",
 
-       "url": "http://192.168.36.50:81/api/v1/evo/config/obj/remote",
-    "user": "systemapi",
-    "password": "tiws2019",
-    "timeout": 120,
-    "verify": "False",
-    "platform_id": 2,
-          "mysql_table": "bifrost_terminal_test",
-          "mongo_normalization": "puller",
-    "mongo_limit_time": 55,
-    "mongo_collection": "idirect_test_lima",
-    "primary_join_cols": {"mysql":"siteId","mongo":"siteId","platform":"Name","old":"Name"},
-    "secondary_join_cols": {"mysql":[
-        "mysql_siteId",
-        "mysql_id_nms"
-      ],"mongo":[
-        "mongo_Name",
-        "mongo_ID"
-      ],"platform":[
-        "platform_Name",
-        "platform_ID"
-      ],"old":[
-        "old_Name",
-        "old_ID"
-      ]},
-    "platform_name": "idirect_lima"}
-    ]
 # [END import_module]
 
 # [START default_args]
@@ -59,7 +30,7 @@ default_args = {
 
 # [START instantiate_dag]
 @dag(default_args=default_args, schedule_interval=None, start_date=days_ago(2), tags=['idirect_lima'])
-def puller(config):
+def puller():
     """
     ### TaskFlow API Tutorial Documentation
     This is a simple ETL data pipeline example which demonstrates the use of
@@ -89,14 +60,45 @@ def puller(config):
     # [END extract]
 
     # [START main_flow]
-    key_process = str(config["platform_id"])+"-"+str( config["platform_name"])
+    
+    config = [
+      {
+              "route_trunk": "data",
+
+           "url": "http://192.168.36.50:81/api/v1/evo/config/obj/remote",
+        "user": "systemapi",
+        "password": "tiws2019",
+        "timeout": 120,
+        "verify": "False",
+        "platform_id": 2,
+              "mysql_table": "bifrost_terminal_test",
+              "mongo_normalization": "puller",
+        "mongo_limit_time": 55,
+        "mongo_collection": "idirect_test_lima",
+        "primary_join_cols": {"mysql":"siteId","mongo":"siteId","platform":"Name","old":"Name"},
+        "secondary_join_cols": {"mysql":[
+            "mysql_siteId",
+            "mysql_id_nms"
+          ],"mongo":[
+            "mongo_Name",
+            "mongo_ID"
+          ],"platform":[
+            "platform_Name",
+            "platform_ID"
+          ],"old":[
+            "old_Name",
+            "old_ID"
+          ]},
+        "platform_name": "idirect_lima"}
+    ]
+    key_process = str(config["platform_id"])+"-"+str(config["platform_name"])
     print("---------------- OLD ---------")
     order_data = extractDataOld(key_process)
     # [END main_flow]
 
 
 # [START dag_invocation]
-puller = puller(config)
+puller = puller()
 # [END dag_invocation]
 
 # [END tutorial]
