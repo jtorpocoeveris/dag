f5ef6a56aded7484bca2f751ebd230e4aef32f53
root <root@DESKTOP-28JH53O.localdomain>
2021-07-08 07:57:00 -0500

    adddag

--- dags/puller.py
+++ dags/puller.py
@@ -143,8 +143,9 @@ def puller():
     mysql_data = extract_mysql(key_process)
     comparate_old_vs_new = comparate_old_vs_new()
     # load(order_data,platform_data)
-    [platform_data,old_data,mongo_data,mysql_data]
-    comparate_old_vs_new
+    [platform_data,old_data] >> comparate_old_vs_new
+    mongo_data
+    mysql_data
     # [END main_flow]
 
 
