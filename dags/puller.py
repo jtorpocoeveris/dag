eb0db990b90cfd56fbdd5dc17915105abd7f0c4f
root <root@DESKTOP-28JH53O.localdomain>
2021-07-08 16:07:31 -0500

    ok

--- dags/puller.py
+++ dags/puller.py
@@ -223,7 +223,7 @@ def puller_idirect():
     def comparate_primary_mysql(df_mysql,comparate):
         df_mysql = pd.DataFrame(json.loads(df_mysql))
         comparate = pd.DataFrame(json.loads(comparate['comparation']))
-        platform_data = pd.DataFrame(json.loads(comparate['platform_data']))
+        platform_data = pd.DataFrame(comparate['platform_data'])
         both = comparate[comparate['_merge_']=='both']
     # def comparate_primary_mysql(both,df_mysql,df_plat):
         both['exist_mysql'] = np.where(both['concat_key_generate'].isin(list(df_mysql['concat_key_generate'])) , 1, 0)
