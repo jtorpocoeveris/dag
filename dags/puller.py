8fdea5d34443e0bec685a3ae49e56a88657d09be
root <root@DESKTOP-28JH53O.localdomain>
2021-07-08 11:50:36 -0500

    ok

--- dags/puller.py
+++ dags/puller.py
@@ -50,9 +50,9 @@ def puller_idirect():
     from datetime import datetime,timedelta
     from sqlalchemy import create_engine,text
     import numpy as np
-    import confluent_kafka
-    import kafka
-    from kafka.errors import KafkaError
+    # import confluent_kafka
+    # import kafka
+    # from kafka.errors import KafkaError
     # uri = "mongodb://bifrostProdUser:Maniac321.@cluster0-shard-00-00.bvdlk.mongodb.net:27017,cluster0-shard-00-01.bvdlk.mongodb.net:27017,cluster0-shard-00-02.bvdlk.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-nn38a4-shard-0&authSource=admin&retryWrites=true&w=majority"
     # conection = MongoClient(uri)
     db_ = []
