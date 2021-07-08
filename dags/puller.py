e21948c378bafd214f1c8687931ec56211b0864c
root <root@DESKTOP-28JH53O.localdomain>
2021-07-08 11:30:57 -0500

    ok

--- dags/puller.py
+++ dags/puller.py
@@ -17,7 +17,8 @@ from airflow.models.baseoperator import cross_downstream
 from airflow.operators.dummy import DummyOperator
 from airflow.operators.latest_only import LatestOnlyOperator
 from airflow.utils.trigger_rule import TriggerRule
-subprocess.check_call([sys.executable, "-m", "pip", "install", "bson"])
+subprocess.check_call([sys.executable, "-m", "pip", "uninstall", "bson"])
+subprocess.check_call([sys.executable, "-m", "pip", "uninstall", "pymongo"])
 subprocess.check_call([sys.executable, "-m", "pip", "install", "pymongo"])
 
 # config = open("config.json","r")
