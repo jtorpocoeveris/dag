550c0b56142112a24367880e24f09eb53e3381b5
root <root@DESKTOP-28JH53O.localdomain>
2021-07-08 21:53:06 -0500

    addTaskfu

--- dags/puller.py
+++ dags/puller.py
@@ -48,8 +48,8 @@ default_args = {
 # start_date=days_ago(2)
 
 # [START instantiate_dag]
-@dag(default_args=default_args, schedule_interval=None, start_date=days_ago(2), tags=['idirect_lima'])
-# @dag(default_args=default_args, schedule_interval='*/10 * * * *', start_date=datetime(2021, 7, 8, 0, 0), tags=['idirect_lima'])
+# @dag(default_args=default_args, schedule_interval=None, start_date=days_ago(2), tags=['idirect_lima'])
+@dag(default_args=default_args, schedule_interval='*/10 * * * *', start_date=datetime(2021, 7, 8, 0, 0), tags=['idirect_lima'])
 def puller_idirect():
     # sys.path.insert(0,os.path.abspath(os.path.dirname(__file__)))
 
