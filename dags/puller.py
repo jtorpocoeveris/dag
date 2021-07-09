31db5a8ae55582b7b8da9b7f4976b21ad08e2bbb
root <root@DESKTOP-28JH53O.localdomain>
2021-07-08 21:19:40 -0500

    addTaskfu

--- dags/puller.py
+++ dags/puller.py
@@ -403,7 +403,7 @@ def puller_idirect():
     comp >> primary_vs_mysql >> send_qq_insert_vsmysql
     comp >> primary_vs_mongo >> send_qq_insert_vsmongo
     primary_vs_mysql >> secondary_vs_mysql >> send_qq
-    # primary_vs_mongo >> secondary_vs_mongo >> send_qq_mongo
+    primary_vs_mongo >> secondary_vs_mongo >> send_qq_mongo
     # old_vs_new
     # old_vs_new >> comparate_primary_mysql(old_vs_new['both'], extract_mysql(engine,config)['data'],old_vs_new['platform_data'])
     # primary_vs_mysql
