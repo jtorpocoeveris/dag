1fd2f43c5ad33ab3cf5993e6449d6e774c1f52cb
root <root@DESKTOP-28JH53O.localdomain>
2021-07-16 17:11:01 -0500

    editOk

--- dags/julioprueba.py
+++ dags/julioprueba.py
@@ -1,3 +1,4 @@
+
 from datetime import timedelta
 from textwrap import dedent
 
@@ -54,3 +55,34 @@ with DAG(
     )
     t1.doc_md = dedent(
         """\
+    #### Task Documentation
+    You can document your task using the attributes `doc_md` (markdown),
+    `doc` (plain text), `doc_rst`, `doc_json`, `doc_yaml` which gets
+    rendered in the UI's Task Instance Details page.
+    ![img](http://montcs.bloomu.edu/~bobmon/Semesters/2012-01/491/import%20soul.png)
+
+    """
+    )
+
+    dag.doc_md = __doc__  # providing that you have a docstring at the beggining of the DAG
+    dag.doc_md = """
+    This is a documentation placed anywhere
+    """  # otherwise, type it like this
+    templated_command = dedent(
+        """
+    {% for i in range(5) %}
+        echo "{{ ds }}"
+        echo "{{ macros.ds_add(ds, 7)}}"
+        echo "{{ params.my_param }}"
+    {% endfor %}
+    """
+    )
+
+    t3 = BashOperator(
+        task_id='templated',
+        depends_on_past=False,
+        bash_command=templated_command,
+        params={'my_param': 'Parameter I passed in'},
+    )
+
+    t1 >> [t2, t3]
