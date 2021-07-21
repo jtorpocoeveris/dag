4a0ba63668cefe8e280abc7cd2a258f4a5dd1a8a
root <root@DESKTOP-28JH53O.localdomain>
2021-07-21 14:56:46 -0500

    addp

--- dags/puller.py
+++ dags/puller.py
@@ -294,25 +294,22 @@ def puller_idirect():
         platform_data = pd.DataFrame(json.loads(comparate['platform_data']))
         comparate = pd.DataFrame(json.loads(comparate['both']))
         both = comparate
-        print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
     # def comparate_primary_mysql(both,df_mysql,df_plat):
         both['exist_mysql'] = np.where(both['concat_key_generate'].isin(list(df_mysql['concat_key_generate'])) , 1, 0)
         exist_mysql_p = both[both['exist_mysql']==1]
         not_exist_mysql_p = both[both['exist_mysql']==0]
         exist_mysql_p = platform_data[platform_data['concat_key_generate'].isin(list(exist_mysql_p['concat_key_generate']))]
+        print("exist_mysql_p")
+        print(exist_mysql_p)
+        print("not_exist_mysql_p")
+        print(not_exist_mysql_p)
         return {'exist_mysql':exist_mysql_p.to_json(orient="records"),'not_exist_mysql':not_exist_mysql_p.to_json(orient="records")}
     
     @task()
     def comparate_primary_mongo(df_mongo,comparate):
         df_mongo = pd.DataFrame(df_mongo)
-        print("df_mongo")
-        print(df_mongo)
         platform_data = pd.DataFrame(json.loads(comparate['platform_data']))
-        print("platform_data")
-        print(platform_data)
         both = pd.DataFrame(json.loads(comparate['both']))
-        print("both")
-        print(both)
         # except:
         #     comparate = pd.DataFrame(columns=['concat_key_generate'])
         # both = comparate
@@ -320,6 +317,8 @@ def puller_idirect():
         both['exist_mongo'] = np.where(both['concat_key_generate'].isin(list(df_mongo['concat_key_generate'])) , 1, 0)
         exist_mongo_p = both[both['exist_mongo']==1]
         exist_mongo_p = platform_data[platform_data['concat_key_generate'].isin(list(exist_mongo_p['concat_key_generate']))]
+        print("exist_mongo_p")
+        print(exist_mongo_p)
         return exist_mongo_p.to_json(orient="records")
 
 
