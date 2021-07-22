b670c521d646de63710f15dd2a41213e974f52d5
root <root@DESKTOP-28JH53O.localdomain>
2021-07-21 19:53:50 -0500

    editOk

--- dags/puller.py
+++ dags/puller.py
@@ -300,10 +300,7 @@ def puller_idirect():
         exist_mysql_p = both[both['exist_mysql']==1]
         not_exist_mysql_p = both[both['exist_mysql']==0]
         exist_mysql_p = platform_data[platform_data['concat_key_generate'].isin(list(exist_mysql_p['concat_key_generate']))]
-        print("exist_mysql_p")
-        print(exist_mysql_p)
-        print("not_exist_mysql_p")
-        print(not_exist_mysql_p)
+        not_exist_mysql_p = platform_data[platform_data['concat_key_generate'].isin(list(not_exist_mysql_p['concat_key_generate']))]
 
         if exist_mysql_p.empty:
             exist_mysql_p=[]
@@ -315,7 +312,10 @@ def puller_idirect():
             not_exist_mysql_p=[]
         else:
             not_exist_mysql_p=not_exist_mysql_p.to_json(orient="records")
-
+        print("exist_mysql_p")
+        print(exist_mysql_p)
+        print("not_exist_mysql_p")
+        print(not_exist_mysql_p)
         return {'exist_mysql':exist_mysql_p,'not_exist_mysql':not_exist_mysql_p}
     
     @task()
@@ -323,8 +323,6 @@ def puller_idirect():
         df_mongo = pd.DataFrame(df_mongo)
         platform_data = pd.DataFrame(json.loads(comparate['platform_data']))
         both = pd.DataFrame(json.loads(comparate['both']))
-        print("both")
-        print(both)
         # except:
         #     comparate = pd.DataFrame(columns=['concat_key_generate'])
         # both = comparate
@@ -333,8 +331,8 @@ def puller_idirect():
         exist_mongo_p = both[both['exist_mongo']==1]
         not_exist_mongo_p = both[both['exist_mongo']==0]
         exist_mongo_p = platform_data[platform_data['concat_key_generate'].isin(list(exist_mongo_p['concat_key_generate']))]
-        print("exist_mongo_p")
-        print(exist_mongo_p)
+        not_exist_mongo_p = platform_data[platform_data['concat_key_generate'].isin(list(not_exist_mongo_p['concat_key_generate']))]
+
         if exist_mongo_p.empty:
             exist_mongo_p=[]
         else:
@@ -346,7 +344,10 @@ def puller_idirect():
         else:
             not_exist_mongo_p=not_exist_mongo_p.to_json(orient="records")
 
-
+        print("exist_mongo_p")
+        print(exist_mongo_p)
+        print("not_exist_mongo_p")
+        print(not_exist_mongo_p)
 
         # return exist_mongo_p.to_json(orient="records")
         return {'exist_mongo':exist_mongo_p,'not_exist_mongo':not_exist_mongo_p}
